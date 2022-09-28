ad6858c0da42f4f1794022c21a83845954a55600
neel123go <ayounpaul1029@gmail.com>
2022-09-28 16:44:24 +0600

    project setup completed

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#6CC6DA",
+          "secondary": "#1246AF",
+          "accent": "#f8f8f8",
+          "neutral": "#333333",
+          "base-100": "#e7e5e4",
+          "success": "#12844F",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
